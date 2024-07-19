2761458fa7d9dddd011ffb38a79d758c7cfa7741
Asim Basak <basak.asim.cs@gmailcom>
2024-07-19 15:34:22 +0530

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='vibration_analysis',
+    name='vibration-analysis',
     version='0.1',
     author='Asim Basak',
     author_email='basak.asim.cs@gmail.com',
