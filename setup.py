3adfdf417b69e5b90d725f5215d6e10b9cbd296f
Asim Basak <basak.asim.cs@gmailcom>
2024-07-19 15:13:03 +0530

    new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vibration_analysis',
+    version='0.1',
+    author='Asim Basak',
+    author_email='basak.asim.cs@gmail.com',
+    description='A brief description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/asimbasak/ant_vibration_analysis',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
